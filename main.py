d95ac178977646f3261cacf0b00763afe7568ff1
habeggaj <habeggaj@miamioh.edu>
2024-01-17 23:57:55 -0500

    Create a framework for the Main.py

--- main.py
+++ main.py
@@ -1,3 +1,52 @@
+import argparse
 
-print("Testing")
+from Identify import valid_xyz_csv
 
+'''
+main.py
+This file is used to run the program.
+'''
+
+
+def execute():
+    '''
+    execute
+    This function is used to execute the program.
+    '''
+    pass
+
+
+def argument_parser():
+    '''
+    argument_parser
+    This function is used to parse the arguments.
+    '''
+    parser = argparse.ArgumentParser(description='This program is used to convert a structure simplification into a '
+                                                 'all atom structure.')
+
+    # Add Arguments
+    parser.add_argument('csv', metavar='csv', type=str, nargs=1, help='The csv file that contains the xyz coordinates '
+                                                                      'of structure simplification.')
+
+    # Execute the parse_args() method
+    user_inputs = parser.parse_args()
+
+    return user_inputs
+
+
+if __name__ == '__main__':
+    # Get Arguments
+    args = argument_parser()
+
+    xyz_csv = args.csv
+
+    if xyz_csv is None:
+        print('Error: No csv file was given.')
+        exit(1)
+
+    if valid_xyz_csv(xyz_csv) is False:
+        print('Error: The csv file given is not a valid xyz csv file.')
+        exit(1)
+
+    # Execute Program
+    execute()
