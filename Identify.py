7df7973a5de8bfdaf794c7c25169e81aaec4561e
habeggaj <habeggaj@miamioh.edu>
2024-01-17 23:43:08 -0500

    Create framework for Identify.py to identify structure using XYZ

---
+++ Identify.py
@@ -0,0 +1,23 @@
+'''
+Identify.py
+This file is used to identify the type of structure that is being
+used given the XYZ coordinates of the atoms in the structure.
+'''
+
+
+def get_xyz(file):
+    '''
+    get_xyz
+    This function is used to get the XYZ coordinates of the atoms
+    in the structure.
+    '''
+    pass
+
+
+def idenify_structure(xyz):
+    '''
+    identify_structure
+    This function is used to identify the type of structure that is
+    being used given the XYZ coordinates of the atoms in the structure.
+    '''
+    pass
