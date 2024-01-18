8cd563fcc22ec5a54ad9085454b96d8810b5b4f0
habeggaj <habeggaj@miamioh.edu>
2024-01-18 00:16:00 -0500

    Convert csv to dataframe to prove valid

--- Identify.py
+++ Identify.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 """
 Identify.py
 This file is used to identify the type of structure that is being
@@ -11,7 +13,22 @@ def valid_xyz_csv(xyz_csv):
     This function is used to check if the given csv file is a valid
     xyz csv file.
     """
-    pass
+
+    # Check if xyz_csv is a csv file
+    if xyz_csv[-4:] != '.csv':
+        print('Error: The file given is not a csv file.')
+        return False
+
+    # Create a dataframe from the csv file
+    try:
+        df = pd.read_csv(xyz_csv)
+        if df.shape[1] == 3:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
 
 
 def get_xyz(file):
