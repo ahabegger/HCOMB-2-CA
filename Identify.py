4f8c53f56e72f9b8f72f3dacd698774dd02227d4
habeggaj <habeggaj@miamioh.edu>
2024-01-18 00:08:30 -0500

    Create valid function for xyz

--- Identify.py
+++ Identify.py
@@ -1,23 +1,32 @@
-'''
+"""
 Identify.py
 This file is used to identify the type of structure that is being
 used given the XYZ coordinates of the atoms in the structure.
-'''
+"""
 
 
-def get_xyz(file):
-    '''
-    get_xyz
-    This function is used to get the XYZ coordinates of the atoms
-    in the structure.
-    '''
+def valid_xyz_csv(xyz_csv):
+    """
+    valid_xyz_csv
+    This function is used to check if the given csv file is a valid
+    xyz csv file.
+    """
     pass
 
 
-def idenify_structure(xyz):
-    '''
+def get_xyz(file):
+    """
+    get_xyz
+    This function is used to get the XYZ coordinates of the atoms
+    in the structure.
+    """
+    pass
+
+
+def identify_structure(xyz):
+    """
     identify_structure
     This function is used to identify the type of structure that is
     being used given the XYZ coordinates of the atoms in the structure.
-    '''
+    """
     pass
